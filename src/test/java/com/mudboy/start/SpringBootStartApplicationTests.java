62df6352f19f58204b7ee9d33df2eb4b6f368151
seungtae2.lee <seungtae2.lee@GR-RD10-NE10049.LGE.NET>
2020-01-13 11:47:43 +0900

    Spring Boot Start

---
+++ src/test/java/com/mudboy/start/SpringBootStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudboy.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
